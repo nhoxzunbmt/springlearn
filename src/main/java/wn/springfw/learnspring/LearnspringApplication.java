5f620a500aa69682724b532ebb8d6da8975df70b
Thanh Loi Tran <loi.tran@fitin.vn>
2020-07-10 09:37:14 +0700

    first commit

---
+++ src/main/java/wn/springfw/learnspring/LearnspringApplication.java
@@ -0,0 +1,13 @@
+package wn.springfw.learnspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnspringApplication.class, args);
+	}
+
+}
